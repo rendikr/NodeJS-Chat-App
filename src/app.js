66a8f39dc4cb742bed5afc98110acaeb616870a8
Rendi K <rendi.krinaldi@gmail.com>
2020-10-27 00:40:35 +0700

    creating the chat app project

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+const path = require('path')
 const express = require('express')
+
+const publicDirectoryPath = path.join(__dirname, '../public')
 
 const app = express()
 
 app.use(express.json()) // automatically parse any json
+app.use(express.static(publicDirectoryPath))
 
 module.exports = app
