2c272de381e612237d1625f95131fc2a968b28db
Rendi K <rendi.krinaldi@gmail.com>
2020-10-28 11:27:34 +0700

    socket.io broadcast events (user joined & leave the chat room)

--- src/index.js
+++ src/index.js
@@ -10,9 +10,14 @@ io.on('connection', (socket) => {
   console.log('New WebSocket connection')
 
   socket.emit('message', 'Welcome!')
+  socket.broadcast.emit('message', 'A new user has joined the chat room!')
 
   socket.on('sendMessage', (message) => {
     io.emit('message', message)
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat room')
   })
 })
 
